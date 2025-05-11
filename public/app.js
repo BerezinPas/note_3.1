2fe70d8e917859e6729be07b04e5d6d3a6b1405d
Berezin <pas.1999@mail.ru>
2025-05-11 13:55:35 +0700

    third

--- public/app.js
+++ public/app.js
@@ -8,16 +8,42 @@ document.addEventListener("click", (event) => {
   }
 
   if (event.target.dataset.type === "edit") {
-    const newTitle = prompt("Введите новвое название");
+    const noteItem = event.target.closest(".list-group-item");
+    const noteForm = noteItem.querySelector("[data-update-form]");
+    const noteViewItem = noteItem.querySelector("[data-view-item]");
+
+    noteForm.classList.remove("d-none");
+    noteViewItem.classList.add("d-none");
+  }
+
+  if (event.target.dataset.type === "save") {
+    const id = event.target.dataset.id;
+    const noteItem = event.target.closest(".list-group-item");
+    const newTitle = noteItem.querySelector('input[name="update_title"]').value;
+    console.log(newTitle);
+
     if (newTitle === null) {
       return;
     }
-    const id = event.target.dataset.id;
     update({ id, title: newTitle }).then(() => {
       event.target.closest("li").querySelector("span").innerText = newTitle;
+      closeForm(noteItem);
     });
   }
+
+  if (event.target.dataset.type === "cancel") {
+    const noteItem = event.target.closest(".list-group-item");
+    closeForm(noteItem);
+  }
 });
+
+function closeForm(noteItem) {
+  const noteForm = noteItem.querySelector("[data-update-form]");
+  const noteViewItem = noteItem.querySelector("[data-view-item]");
+
+  noteForm.classList.add("d-none");
+  noteViewItem.classList.remove("d-none");
+}
 
 async function remove(id) {
   await fetch(`/${id}`, { method: "DELETE" });
